e04c8b66315d2abf2a9df1c42e0fd95c3f296177
ddayin <ddayin@hotmail.com>
2020-08-28 22:52:10 +0900

    스크롤뷰 추가

--- Assets/Scripts/Panel_Shop.cs
+++ Assets/Scripts/Panel_Shop.cs
@@ -9,6 +9,8 @@ namespace CommonShop
     {
         private Text m_TextTitle;
         private Button m_ButtonClose;
+        [SerializeField] private GameObject m_ShopItemPrefab;
+        [SerializeField] private Transform m_TransformParent;
 
         private void Awake()
         {
@@ -19,10 +21,35 @@ namespace CommonShop
             m_ButtonClose.onClick.AddListener(OnClickClose);
         }
 
+        private void Start()
+        {
+            TestCreate();
+        }
+
+        private void TestCreate()
+        {
+            for (int i = 0; i < 50; i++)
+            {
+                InstantiateItem();
+            }
+        }
+
         private void OnClickClose()
         {
             //Destroy(this.gameObject);
             PopupManager.Instance.CloseShop();
+        }
+
+        /// <summary>
+        /// 상점 아이템을 instantiate해서 새롭게 생성하고 ShopItem을 반환한다.
+        /// </summary>
+        /// <returns></returns>
+        private ShopItem InstantiateItem()
+        {
+            GameObject newObject = Instantiate(m_ShopItemPrefab) as GameObject;
+            newObject.transform.SetParent(m_TransformParent);
+            ShopItem item = newObject.GetComponent<ShopItem>();
+            return item;
         }
     }
 }
---
+++ Assets/Scripts/ShopItem.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace CommonShop
+{
+    /// <summary>
+    /// 스크롤뷰 안에 들어갈 상점 아이템
+    /// </summary>
+    public class ShopItem : MonoBehaviour
+    {
+        private Button m_Button;
+        private Image m_Image;
+
+        private void Awake()
+        {
+            m_Button = transform.Find("Button").GetComponent<Button>();
+            m_Image = transform.GetComponent<Image>();
+
+            m_Button.onClick.AddListener(OnClickButton);
+        }
+
+        private void OnClickButton()
+        {
+            Debug.Log("OnClickButton() called");
+        }
+    }
+}
