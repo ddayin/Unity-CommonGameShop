90c3dcb1b50e0c71d589a2741044af0bcdfaa93f
ddayin <ddayin@hotmail.com>
2020-09-20 20:54:23 +0900

    리팩토링

--- Assets/Scripts/ResourcesLoader.cs
+++ Assets/Scripts/ResourcesLoader.cs
@@ -10,11 +10,6 @@ namespace CommonShop
     /// </summary>
     public class ResourcesLoader : MonoSingleton<ResourcesLoader>
     {
-        /// <summary>
-        /// key : id, value : Sprite 인 상점의 아이템을 담은 딕셔너리
-        /// </summary>
-        public Dictionary<string, Sprite> m_ShopItemsDic = new Dictionary<string, Sprite>();
-
         public List<Sprite> m_ShopSpritesList = new List<Sprite>();
 
         public ShopData m_ShopData;
@@ -24,7 +19,6 @@ namespace CommonShop
             LoadShopItemSprites();
         }
 
-
         /// <summary>
         /// 상점 아이템 스프라이트들을 불러옵니다.
         /// </summary>
@@ -32,7 +26,6 @@ namespace CommonShop
         {
             Sprite[] sprites = Resources.LoadAll<Sprite>("Textures/ShopItem") as Sprite[];
 
-            int i = 0;
             foreach (Sprite sp in sprites)
             {
                 m_ShopSpritesList.Add(sp);
