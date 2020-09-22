5a636d7fe8d5b1f84bdcb840114085c3a358eda1
ddayin <ddayin@hotmail.com>
2020-09-22 20:29:07 +0900

    category 추가 중

--- Assets/Scripts/Category_Controller.cs
+++ Assets/Scripts/Category_Controller.cs
@@ -7,6 +7,7 @@ namespace CommonShop
     public class Category_Controller : MonoBehaviour
     {
         [SerializeField] private List<Item_Category> m_CategoryList = new List<Item_Category>();
+        [SerializeField] private List<RectTransform> m_ScrollList = new List<RectTransform>();
 
         private void Awake()
         {
@@ -16,11 +17,29 @@ namespace CommonShop
             m_CategoryList[3].Setup(ShopCategory.DDD);
 
             m_CategoryList[0].CategoryButton.onClick.AddListener(OnClickAAA);
+            m_CategoryList[1].CategoryButton.onClick.AddListener(OnClickBBB);
+            m_CategoryList[2].CategoryButton.onClick.AddListener(OnClickCCC);
+            m_CategoryList[3].CategoryButton.onClick.AddListener(OnClickDDD);
         }
 
         private void OnClickAAA()
         {
             
+        }
+
+        private void OnClickBBB()
+        {
+
+        }
+
+        private void OnClickCCC()
+        {
+
+        }
+
+        private void OnClickDDD()
+        {
+
         }
     }
 }
